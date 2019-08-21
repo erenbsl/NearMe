a312ad12e472c464655e9b078d7e5966491d3c7a
Eren Besel <eren.besel@smartnews.com>
2019-08-21 11:19:39 +0900

    Initial Commit

---
+++ NearMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Eren Besel on 2019/08/21.
+//  Copyright © 2019 Eren Besel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
