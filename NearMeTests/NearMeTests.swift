3b6168bdd9c484c5e07c5994f6489802874f8fe3
Eren Besel <erenbesel@gmail.com>
2019-08-23 19:13:13 +0900

    Sample unit tests to demonstrate the ease.

--- NearMeTests/NearMeTests.swift
+++ NearMeTests/NearMeTests.swift
@@ -11,17 +11,50 @@ import XCTest
 
 class NearMeTests: XCTestCase {
 
+    var venueViewModel: VenueListViewModel!
+    var filterViewModel: FilterViewModel!
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        venueViewModel = VenueListViewModel(dataProvider: VenuesDataProvider())
+        filterViewModel = FilterViewModel(filter: Filter(keyword: "test", radius: 100))
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testVenueViewModel() {
+        var venuesResponse: Response<RecommendedVenuesResponse>
+        let bundle = Bundle(for: NearMeTests.classForCoder())
+        if let path = bundle.path(forResource: "Venues", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+                venuesResponse = try JSONDecoder().decode(Response<RecommendedVenuesResponse>.self, from: data)
+                venueViewModel.venues = venuesResponse.responseObject.groups.first?.items ?? []
+                
+                XCTAssertTrue(venueViewModel.numberOfVenues == 10)
+                
+                let cellModel = venueViewModel.cellModel(at: 5)
+                XCTAssertNotNil(cellModel)
+                
+                let cellModel2 = venueViewModel.cellModel(at: 11)
+                XCTAssertNil(cellModel2)
+            } catch let error {
+                XCTFail("parse error: \(error.localizedDescription)")
+            }
+        } else {
+            XCTFail("Invalid filename/path.")
+        }
+    }
+    
+    func testFilterViewModel() {
+        XCTAssert(filterViewModel.keyword == "test")
+        XCTAssert(filterViewModel.radius == 100)
+        
+        filterViewModel.updateValues(keyword: "test2", radius: 1000)
+        XCTAssert(filterViewModel.keyword == "test2")
+        XCTAssert(filterViewModel.radius == 1000)
     }
 
     func testPerformanceExample() {
